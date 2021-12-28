721ad6da12acef1faea481594989140e9fd55a21
Vova <toed2020@mail.ru>
2021-12-28 19:34:49 +0300

    N1

---
+++ src/test/java/com/codelike/jnews/JnewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codelike.jnews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JnewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
